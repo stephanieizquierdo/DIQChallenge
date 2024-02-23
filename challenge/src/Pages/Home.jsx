d982fcc734270b4fc6250d1e26dcc3ece49a09db
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2024-02-23 19:19:15 -0300

    fix grid

--- challenge/src/Pages/Home.jsx
+++ challenge/src/Pages/Home.jsx
@@ -2,10 +2,11 @@ import React from "react"
 import { useEffect, useState, useCallback } from "react";
 import UserCard from "../Components/UserCard";
 import getUsers from "../Services/usersService";
+import { Container, Grid, CircularProgress} from '@mui/material';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
-
+    const [isLoading, setLoading] = useState(true)
     const fetchData = useCallback(async()=> {
         const data = await getUsers();
         setUsers(data)
@@ -13,20 +14,42 @@ const Home = () => {
     
     useEffect(() => {
         fetchData()
+        setLoading(false)
     }, [fetchData]);
-    
+
     return (
-        <div>
-            {console.log(users)}
-                <h2 style={{textAlign:"center"}}> Users </h2>
-                <div style={{display:"flex", justifyContent:"space-around", margin:"auto"}}>
-                        {users?.map( (usuario, index) => {
-                                return(<UserCard key={index} user={usuario}/>);
-                            })
-                        }
-                </div>
-                
-            </div>
+        <Container style={{margin: "5%"}}>
+            <h2 style={{textAlign:"center"}}> Users </h2>
+
+            {isLoading?
+                <CircularProgress 
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: '100vh',
+                        margin: 'auto',
+                        width: '10vw'
+                    }}
+                />
+                :
+                <Grid
+                    container
+                    spacing={5}
+                    style={{justifyContent: "center"}}
+                >
+                {users?.map( (usuario, index) => {
+                        return(
+                            <Grid item key={index}>
+                                <UserCard key={index} user={usuario}/>
+                            </Grid>
+                        );
+                    })
+                }
+                </Grid>
+            }
+        </Container>
+
         );
 }
 
